7ad3cc2f63bab7000de8a04608f25a57782e2a5a
Li Wei <oldrev@gmail.com>
2021-09-26 13:20:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# XamlWinForm - A XAML WinForm Demo App
+
+A demostration program for creating a XAML based WinForm app with .NET Core 3.
+
+## Getting Started
+
+Just open the `.sln` file with Visual Studio 2019+ and run.
---
+++ XamlWinForm/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OswaldTechnologies.Extensions.Hosting.WindowsFormsLifetime;
+using Portable.Xaml;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XamlWinForm
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            CreateHostBuilder().Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder() =>
+            Host.CreateDefaultBuilder(Array.Empty<string>())
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var form = XamlServices.Load("Form1.xaml") as Form;
+
+                    services.AddSingleton<IHostLifetime, WindowsFormsLifetime>();
+                    services.AddSingleton(form);
+                    services.AddHostedService<WindowsFormsHostedService<Form>>();
+                    // services.AddSingleton<IHostBuilder>();
+                });
+    }
+}
